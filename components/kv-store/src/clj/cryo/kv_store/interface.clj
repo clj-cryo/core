dafd41eec827e40266231c0f3cea33bcd1fbf897
Jared <jaredholmberg@hotmail.com>
2024-11-15 07:29:04 -0600

    Stubbing out the kv-store namespace API

---
+++ components/kv-store/src/clj/cryo/kv_store/interface.clj
@@ -0,0 +1,1 @@
+(ns clj.cryo.kv-store.interface)
---
+++ components/kv-store/test/clj/cryo/kv_store/interface_test.clj
@@ -0,0 +1,6 @@
+(ns clj.cryo.kv-store.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [clj.cryo.kv-store.interface :as kv-store]))
+
+(deftest dummy-test
+  (is (= 1 1)))
